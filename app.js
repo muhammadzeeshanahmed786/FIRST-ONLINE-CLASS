00705f74c054d62b129e94b0b058619fc9780e5a
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-03 11:16:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log("zeeshan")
+
+
+
+
+// let num1 = 5;
+// let num2 = 9;
+
+
+// console.log(num1++ + num1-- + ++num1 + ++num1);
+
+
+
+// var pro=prompt("Whats your name?")
+
+// console.log("WellCome !",pro);
+// alert(pro)
+
+
+
+// var userPromt=prompt("what would you like to eat?", "anything fresh")
+
+// alert("Here is Your Favorite Dish " + userPromt)
+
+
+
+var tableName=prompt("Table Name");
+
+for(var i=1; i<=10; i++){
+    document.write(tableName +"X"+ [i] + "=" +tableName*i +"<br>")
+}
+
